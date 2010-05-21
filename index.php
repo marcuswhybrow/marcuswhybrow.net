847d43450481e16bfd90d131af97484879eb985f
Marcus Whybrow <marcus@marcuswhybrow.net>
2010-05-21 22:14:58 +0100

    Added better visual styling to h2 and h3 tags (just like djangoadvent.com), and added better (red) link styles to posts.

--- index.php
+++ index.php
@@ -4,20 +4,21 @@
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<div class="post">
             <p class="post-date small">Written on <?php the_time('F j, Y'); ?> at <?php the_time() ?>, by <?php the_author() ?></p>
-            <h2><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+            <h1><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 
             <?php the_content(__('Read more'));?>
 
-        <div class="postmeta">
-            <p><?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?></p>
-            <!--<p>Category <?php the_category(', ') ?> | Tags: <?php the_tags(' ', ',', ' '); ?></p>
-            <!--
-            Social Networking Links if you're interested. Please note, that the following networking links contain invalid XHTML.
-            If you want more social network links, check out this URL: http://brianp.covie.ws/Zp0
-            -->
-            <p>Social Networks: <a href="http://www.facebook.com/share.php?u=<?php the_permalink(); ?>&t=<?php the_title(); ?>">Facebook</a>, <a href="http://twitter.com/home?status=<?php the_title(); ?> - <?php the_permalink(); ?>">Twitter</a>,  <a href="http://www.google.com/bookmarks/mark?op=edit&bkmk=<?php the_permalink(); ?>&title=<?php the_title(); ?>&annotation=<?php the_excerpt(); ?>">Google Bookmarks</a>, <a href="http://delicious.com/post?url=<?php the_permalink(); ?>&title=<?php the_title(); ?>&notes=<?php the_excerpt(); ?>">del.icio.us</a>, <a href="http://www.stumbleupon.com/submit?url=<?php the_permalink(); ?>&title=<?php the_title(); ?>">StumbleUpon</a>, <a href="http://digg.com/submit?phase=2&url=<?php the_permalink(); ?>&title=<?php the_title(); ?>&bodytext=<?php the_excerpt(); ?>">Digg</a>, <a href="http://posterous.com/share?linkto=<?php the_permalink(); ?>&title=<?php the_title(); ?>&selection=<?php the_excerpt(); ?>">Reddit</a>, <a href="http://posterous.com/share?linkto=<?php the_permalink(); ?>&title=<?php the_title(); ?>&selection=<?php the_excerpt(); ?>">Posterous</a>.</p>-->
-        </div><!-- end .postmeta -->
-    </div><!-- end .post -->
+            <div class="postmeta">
+                <p><?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?></p>
+                <!--<p>Category <?php the_category(', ') ?> | Tags: <?php the_tags(' ', ',', ' '); ?></p>
+                <!--
+                Social Networking Links if you're interested. Please note, that the following networking links contain invalid XHTML.
+                If you want more social network links, check out this URL: http://brianp.covie.ws/Zp0
+                -->
+                <p>Social Networks: <a href="http://www.facebook.com/share.php?u=<?php the_permalink(); ?>&t=<?php the_title(); ?>">Facebook</a>, <a href="http://twitter.com/home?status=<?php the_title(); ?> - <?php the_permalink(); ?>">Twitter</a>,  <a href="http://www.google.com/bookmarks/mark?op=edit&bkmk=<?php the_permalink(); ?>&title=<?php the_title(); ?>&annotation=<?php the_excerpt(); ?>">Google Bookmarks</a>, <a href="http://delicious.com/post?url=<?php the_permalink(); ?>&title=<?php the_title(); ?>&notes=<?php the_excerpt(); ?>">del.icio.us</a>, <a href="http://www.stumbleupon.com/submit?url=<?php the_permalink(); ?>&title=<?php the_title(); ?>">StumbleUpon</a>, <a href="http://digg.com/submit?phase=2&url=<?php the_permalink(); ?>&title=<?php the_title(); ?>&bodytext=<?php the_excerpt(); ?>">Digg</a>, <a href="http://posterous.com/share?linkto=<?php the_permalink(); ?>&title=<?php the_title(); ?>&selection=<?php the_excerpt(); ?>">Reddit</a>, <a href="http://posterous.com/share?linkto=<?php the_permalink(); ?>&title=<?php the_title(); ?>&selection=<?php the_excerpt(); ?>">Posterous</a>.</p>-->
+            </div><!-- end .postmeta -->
+            <hr>
+        </div><!-- end .post -->
 
     <?php endwhile; else: ?>
         <p>
