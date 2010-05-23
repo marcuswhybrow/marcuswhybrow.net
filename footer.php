2070389c49e6de019456324e5e72f75b7ce85870
Marcus Whybrow <marcus@marcuswhybrow.net>
2010-05-23 19:12:32 +0100

    Improved the readability of the outputted source code

--- footer.php
+++ footer.php
@@ -1,22 +1,29 @@
-<div id="footer" class="container">
-    <hr>
+    <div id="footer-wrapper">
+        <div id="footer" class="container">
+            <hr>
 
-    <section class="archives">
-        <h5>Archives</h5>
-        <ul>
-            <?php wp_get_archives('type=monthly'); ?>
-        </ul>
-    </section>
+            <section class="archives">
+                <h5>Archives</h5>
+                <ul>
+<?php wp_get_archives('type=monthly&format=custom&before=            <li>&after=</li>'); ?>
+                </ul>
+            </section>
 
-    <p>
-        <a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>
-    </p>
-    <p>
-        <?php bloginfo('name'); ?> is proudly powered by <a href="http://wordpress.org/">WordPress.org</a> <!-- It is recommended that you leave this link. Links such as these are Wordpress' main form of advertising. --> and
-        <a href="http://whiteboardframework.com">whiteboard</a> <!-- You may remove this link. Though, I would appreciate it if you left it or at least gave me a link somewhere... ;-) -->
-    </p>
- </div><!-- end #footer -->
+            <p>
+                <a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>
+            </p>
+
+            <p class="small">
+                <?php bloginfo('name'); ?> is proudly powered by <a href="http://wordpress.org/">WordPress.org</a>
+                and
+                <a href="http://whiteboardframework.com">whiteboard</a>
+            </p>
+        </div><!-- end #footer -->
+    </div><!-- end #footer-wrapper -->
 </div><!-- end #container -->
 
+<!-- start Wordpress footer -->
 <?php wp_footer(); ?>
+<!-- end Wordpress footer -->
+
 </body></html>
--- header.php
+++ header.php
@@ -1,25 +1,28 @@
 <!DOCTYPE HTML>
 <html <?php language_attributes(); ?>>
-    <head>
-        <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>">
-        <title><?php wp_title('|', true, 'right'); ?> <?php bloginfo('name'); ?> <?php if ( !wp_title('', true, 'left') ); { ?> | <?php bloginfo('description'); ?> <?php } ?></title>
-        <meta name="generator" content="WordPress"> <!-- leave this for stats (or remove for potential security reasons) -->
-        <meta name="author" content="Marcus Whybrow">
+<head>
+    <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>">
+    <title><?php wp_title('|', true, 'right'); ?> <?php bloginfo('name'); ?> <?php if ( !wp_title('', true, 'left') ); { ?> | <?php bloginfo('description'); ?> <?php } ?></title>
+    <meta name="generator" content="WordPress"> <!-- leave this for stats (or remove for potential security reasons) -->
+    <meta name="author" content="Marcus Whybrow">
 
-        <?php wp_head(); ?>	
+    <!-- start Wordpress head -->
+    <?php wp_head(); ?>	
+    <!-- end Wordpress head -->
         
-        <link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/css/blueprint/screen.css" type="text/css" media="screen, projection">
-        <link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/css/blueprint/print.css" type="text/css" media="print">
-        <!--[if lt IE 8]><link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/css/blueprint/ie/css" type="text/css" media="screen, projection"><![endif]-->
-        <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/blueprint/plugins/buttons/screen.css" type="text/css" media="screen, projection">
-        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen, projection">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/css/blueprint/screen.css" type="text/css" media="screen, projection">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/css/blueprint/print.css" type="text/css" media="print">
+    <!--[if lt IE 8]><link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/css/blueprint/ie/css" type="text/css" media="screen, projection"><![endif]-->
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/blueprint/plugins/buttons/screen.css" type="text/css" media="screen, projection">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen, projection">
         
-        <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>">
-        <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+    <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
-        <script type="text/javascript" src="<?php bloginfo('template_directory') ?>/js/global.js"></script>
-    </head>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_directory') ?>/js/global.js"></script>
+
+</head>
 <body>
 
 <div id="container">
@@ -31,6 +34,7 @@
             </div><!-- end #topTitle -->
             <div id="topNav">
                 <?php /* wp_page_menu(); */ ?>
+
             </div><!-- end #topNav -->
             <a href="http://github.com/marcuswhybrow/marcuswhybrow.net/"><img style="position: absolute; top: 0; left: 0; border: 0;" src="http://s3.amazonaws.com/github/ribbons/forkme_left_gray_6d6d6d.png" alt="Fork me on GitHub" /></a>
         </div><!-- end #header -->
