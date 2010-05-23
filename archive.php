b92bb25a20f3495585ae1da97cf248d89375a534
Marcus Whybrow <marcus@marcuswhybrow.net>
2010-05-23 15:14:08 +0100

    Stopped using the "Wordpress Loop" in archives, since a limit is imposed via the admin interface. Instead used the get_posts function.

--- archive.php
+++ archive.php
@@ -3,31 +3,36 @@
 <div id="content" class="container">
     <?php is_tag(); ?>
     <?php if (have_posts()) : ?>
-
         <?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
         <?php /* If this is a category archive */ if (is_category()) { ?>
-            <h1>Archive for the &#8216;<?php single_cat_title(); ?>&#8217; Category</h1>
+            <h2><span><?php single_cat_title(); ?></span> Posts</h2>
+            <?php $picker = 'category_name=' . single_cat_title(); ?>
         <?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
-            <h1>Posts Tagged &#8216;<?php single_tag_title(); ?>&#8217;</h1>
+            <h2>Posts Tagged <span>&#8216;<?php single_tag_title(); ?>&#8217;</span></h2>
+            <?php $picker = 'tag=' . single_tag_title(); ?>
         <?php /* If this is a daily archive */ } elseif (is_day()) { ?>
-            <h1>Archive for <?php the_time('F jS, Y'); ?></h1>
+            <h2>Archive for <span><?php the_time('F jS, Y'); ?></span></h2>
+            <?php $picker = 'year=' . get_the_time('Y') . '&monthnum=' . get_the_time('n') . '&day=' . get_the_time('j'); ?>
         <?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-            <h1>Archive for <?php the_time('F, Y'); ?></h1>
+            <h2>Archive for <span><?php the_time('F, Y'); ?></span></h2>
+            <?php $picker = 'year=' . get_the_time('Y') . '&monthnum=' . get_the_time('n'); ?>
         <?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-            <h1>Archive for <?php the_time('Y'); ?></h1>
+            <h2>Archive for <span><?php the_time('Y'); ?></span></h2>
+            <?php $picker = 'year=' . get_the_time('Y'); ?>
         <?php /* If this is an author archive */ } elseif (is_author()) { ?>
-            <h1>Author Archive</h1>
+            <?php the_post(); $author = get_userdata($post->post_author); ?>
+            <h2>All Posts by <span><?php echo $author->first_name; ?></span></h2>
         <?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-            <h1>Blog Archives</h1>
+            <h2>Blog Archives</h2>
         <?php } ?>
 
         <ul class="posts">
-        <?php $thisPost = ''; $lastPost = ''; ?>
-        <?php while (have_posts()) : the_post(); ?>
-            <?php
+        <?php
+            $thisPost = ''; $lastPost = '';
+            foreach(get_posts('numberposts=-1&orderby=post_date&order=DESC&' . $picker) as $post) :
                 if (is_author()) { $thisPost = get_the_time('Y'); } 
                 elseif (is_year()) { $thisPost = get_the_time('F'); }
-            ?>
+        ?>
             <?php if($thisPost != $lastPost) { ?>
                 </ul>
                 <h3><?php echo $thisPost ?></h3>
@@ -35,7 +40,7 @@
             <?php } ?>
             <li><span><?php the_time('d M Y') ?></span> » <a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></li>
             <?php $lastPost = $thisPost; ?>
-        <?php endwhile; ?>
+        <?php endforeach; ?>
         </ul>
 
         <div id="postnavigation">
